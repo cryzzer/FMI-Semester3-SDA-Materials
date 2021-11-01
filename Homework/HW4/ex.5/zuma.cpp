215239d8661c026bef211acf63224d1763daaee6
Danail Nenkov <dpnenkov29@abv.bg>
2021-11-02 00:42:36 +0200

    isEmpty, addressOfDestroyedBalls and some main logic

--- Homework/HW4/ex.5/zuma.cpp
+++ Homework/HW4/ex.5/zuma.cpp
@@ -16,9 +16,9 @@ class DoublyLinkedList {
   DoublyLinkedListNode* head;
   DoublyLinkedListNode* tail;
 
+ public:
   std::vector<DoublyLinkedListNode*> numberPtrs;
 
- public:
   DoublyLinkedList(int numberCount) : head(nullptr), tail(nullptr) {
     numberPtrs = std::vector<DoublyLinkedListNode*>(numberCount, nullptr);
   }
@@ -29,13 +29,14 @@ class DoublyLinkedList {
           new DoublyLinkedListNode(color, idNumber);
       head = firstNode;
       tail = firstNode;
-      
+
       numberPtrs[idNumber] = firstNode;
       return;
     }
 
     if (head == tail) {
-      DoublyLinkedListNode* secondNode = new DoublyLinkedListNode(color, idNumber);
+      DoublyLinkedListNode* secondNode =
+          new DoublyLinkedListNode(color, idNumber);
       head->next = secondNode;
       secondNode->previous = head;
       tail = secondNode;
@@ -52,26 +53,88 @@ class DoublyLinkedList {
     numberPtrs[idNumber] = newNode;
   }
 
-  void insertNote_atPos(int insertionPos, int color, int idNumber){
-      DoublyLinkedListNode* newNode = new DoublyLinkedListNode(color, idNumber);
-      numberPtrs.push_back(newNode);
+  void insertNote_atPos(int insertionPos, int color, int idNumber) {
+    DoublyLinkedListNode* newNode = new DoublyLinkedListNode(color, idNumber);
+    numberPtrs.push_back(newNode);
 
-      DoublyLinkedListNode* insertionPtr = numberPtrs[insertionPos];
+    DoublyLinkedListNode* insertionPtr = numberPtrs[insertionPos];
 
-      if(insertionPtr == tail){
-          insertionPtr->next = newNode;
-          newNode->previous = insertionPtr;
-          tail = newNode;
-          return;
-      }
-
-      DoublyLinkedListNode* nextElement = insertionPtr->next;
-      nextElement->previous = newNode;
-      newNode->next = nextElement;
-
+    if (insertionPtr == tail) {
       insertionPtr->next = newNode;
       newNode->previous = insertionPtr;
+      tail = newNode;
+      return;
+    }
+
+    DoublyLinkedListNode* nextElement = insertionPtr->next;
+    nextElement->previous = newNode;
+    newNode->next = nextElement;
+
+    insertionPtr->next = newNode;
+    newNode->previous = insertionPtr;
+  }
+
+  bool isEmptyList() {
+    if (head == nullptr && tail == nullptr) {
+      return true;
+    }
+    return false;
   }
 };
 
-int main() {}
+std::pair<DoublyLinkedListNode*, DoublyLinkedListNode*> addressOfDestroyedBalls(
+    DoublyLinkedListNode* hittingBall) {
+  int destroyedBallCounter = 1;
+  DoublyLinkedListNode* rightMovingIndex = hittingBall;
+  DoublyLinkedListNode* leftMovingIndex = hittingBall;
+
+  while (rightMovingIndex->next &&
+         rightMovingIndex->color == rightMovingIndex->next->color) {
+    rightMovingIndex = rightMovingIndex->next;
+    destroyedBallCounter++;
+  }
+  while (leftMovingIndex->previous &&
+         leftMovingIndex->color == leftMovingIndex->previous->color) {
+    leftMovingIndex = leftMovingIndex->previous;
+    destroyedBallCounter++;
+  }
+
+  if (destroyedBallCounter < 3) {
+    return std::pair<DoublyLinkedListNode*, DoublyLinkedListNode*>(nullptr,
+                                                                   nullptr);
+  } else {
+    return std::pair<DoublyLinkedListNode*, DoublyLinkedListNode*>(
+        leftMovingIndex, rightMovingIndex);
+  }
+}
+
+int main() {
+  int ballsCount;
+  std::cin >> ballsCount;
+
+  DoublyLinkedList* list = new DoublyLinkedList(ballsCount);
+  int color;
+  for (int i = 0; i < ballsCount; i++) {
+    std::cin >> color;
+    list->push_backNode(color, i);
+  }
+
+  int shotRequests;
+  std::cin >> shotRequests;
+
+  int shootingPos;
+  int ballIdNum = ballsCount;
+  for (int i = 0; i < shotRequests; i++) {
+    std::cin >> shootingPos >> color;
+
+    if (!list->isEmptyList()) {
+      list->insertNote_atPos(shootingPos, color, ++ballIdNum);
+      std::pair<DoublyLinkedListNode*, DoublyLinkedListNode*> ballsToRemove =
+          addressOfDestroyedBalls(list->numberPtrs[ballIdNum]);
+      
+      if(ballsToRemove.first && ballsToRemove.second){
+        
+      }
+    }
+  }
+}
